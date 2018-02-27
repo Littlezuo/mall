4635c75db7d2250c3938ac3fd2a322bbb81d47fd
Littlezuo <767770939@qq.com>
2018-02-27 09:24:22 +0800

    客户端首页内容管理服务

--- manager/interface/src/main/java/cn/e3mall/service/ItemService.java
+++ manager/interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -2,6 +2,7 @@ package cn.e3mall.service;
 
 
 import cn.e3mall.common.pojo.EasyUIDataGridResult;
+import cn.e3mall.common.utils.E3Result;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
@@ -9,4 +10,8 @@ public interface ItemService {
     TbItem getItemById(long itemId);
 
     EasyUIDataGridResult getItemList(int page, int rows);
+
+    E3Result addItem(TbItem item,String desc);
+
+    E3Result editItem(long id);
 }
